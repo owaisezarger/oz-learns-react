6c1432ab14c0f991de8dad2be113607dffee8f55
owaisezarger <owaisezarger@gmail.com>
2024-01-01 20:44:52 +0530

    separated js file to write react code

---
+++ index.js
@@ -0,0 +1,8 @@
+const heading = React.createElement(
+  "h1",
+  {},
+  "Hello World! I am Learning React in 2024"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
