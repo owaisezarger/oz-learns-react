1979b265016a940e70d66f0fc7984bd05a7ef727
owaisezarger <owaisezarger@gmail.com>
2024-01-10 23:38:44 +0530

    elemets inside component

--- App.js
+++ App.js
@@ -5,16 +5,14 @@ const Title = () => {
   return <h1 className="title">This is Title</h1>;
 };
 
-const number = 9321;
-
+const elem = <p>It is an element</p>;
 const Heading = () => {
   return (
     <div>
       <Title />
-      {number + number}
-
       <h2> This is Heading</h2>
       <p>Writing Functional React Components</p>
+      {elem}
     </div>
   );
 };
